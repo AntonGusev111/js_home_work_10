cd89ffd5534a62c0cd21d35ae21db1824aafd9bc
GusevADresume <resumeGusevAD@gmail.com>
2022-08-16 00:20:21 +0300

    v2

--- todo/task.js
+++ todo/task.js
@@ -1,23 +1,31 @@
 function taskHandler(){
-    document.addEventListener('keydown', event => {
+    const btn = document.getElementById('tasks__add')
+    btn.addEventListener('click', event => {
         let input = document.querySelector('.tasks__input').value;
-        if (input != '' && event.key == 'Enter'){
-            addTask(input)
-            return false
-        }
-        return false
+        if (input.trim() != '' ){
+            addTask(input);
+            document.querySelector('.tasks__input').value = '';
+            event.preventDefault(); 
+            let cross =  document.querySelectorAll('.task__remove');
+            for (let i=0; i < cross.length; i++){
+                cross[i].onclick = function() {
+                    cross[i].parentElement.remove(); // Мне очень не нравится как получилось реализовать удаление, прошу дать комментарий, как это сделать иначе. Благодарю за ответ
+                  }
+            }
+        } 
     })
-    return false
 }
+
 
 function addTask(input){
     const list = document.getElementById('tasks__list');
     let task = document.createElement('div');
-    task.innerHTML = "<div class='task'><div class='task__title'>"+input+"</div></div>";
+    task.innerHTML = "<div class='task'><div class='task__title'>"+input+"</div><a href='#' class='task__remove'>&times</a></div>";
     list.insertBefore(task, null)
     return false
 }
 
-//Прошу объяснить, почему страница обновляется и как этого избежать
+
+
 
 taskHandler()
--- tooltip/task.js
+++ tooltip/task.js
@@ -3,17 +3,16 @@ function prompt(){
     let tip = document.querySelector('.tooltip')
     for (let i = 0; i < prompts.length; i++){
         prompts[i].addEventListener("click", function(e){
+
             const {top, left} = prompts[i].getBoundingClientRect();
             tip.textContent = prompts[i].title;
-            tip.style[0] = left;
-            tip.style[1] = top;
-            tip.className = 'tooltip_active';
-            return false
+            tip.style.left = String(left)+ 'px';
+            tip.style.top = String(top+20)+'px';
+            tip.className = 'tooltip tooltip_active';
+            e.preventDefault()
         })
-        return false
     }
-    return false
 }
 
-//Прошу подсказать почему подсказки не показываются и страница обновляется и как это исправить
+
 prompt()
